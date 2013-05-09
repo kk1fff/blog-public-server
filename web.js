59a6c19f0cf3e2d011509baf812bee70e73b99c4
kk1fff <kk1fff@patrickz.net>
2013-05-10 01:52:20 +0800

    remove hello world from web.js

--- web.js
+++ web.js
@@ -3,12 +3,7 @@ var CONF = require('./config.js'),
     express = require('express'),
     app = express();
 
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-});
+app.use(express['static'](__dirname + '/pages'));
 
 exports.startServer = function() {
   l.l('Listening on port: ' + CONF.get('WEB_PORT'));
